7e25f03aa8f578885f6e5ca738d19480e0899b8f
Baptiste Langlade <baptouuuu@gmail.com>
2018-09-25 21:12:34 +0200

    add event structure

---
+++ src/Event.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\GuiltySparkMonitor;
+
+use Innmind\GuiltySparkMonitor\Event\Name;
+use Innmind\Immutable\MapInterface;
+
+final class Event
+{
+    private $name;
+    private $payload;
+
+    public function __construct(Name $name, MapInterface $payload)
+    {
+        if (
+            (string) $payload->keyType() !== 'string' ||
+            (string) $payload->valueType() !== 'variable'
+        ) {
+            throw new \TypeError('Argument 2 must be of type MapInterface<string, variable>');
+        }
+
+        $this->name = $name;
+        $this->payload = $payload;
+    }
+
+    public function name(): Name
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return MapInterface<string, variable>
+     */
+    public function payload(): MapInterface
+    {
+        return $this->payload;
+    }
+}
---
+++ src/Exception/Exception.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\GuiltySparkMonitor\Exception;
+
+interface Exception extends \Throwable
+{
+}
