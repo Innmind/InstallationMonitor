4d3504e1644dd20eb05ff170879a326fd71d11ed
Baptiste Langlade <baptouuuu@gmail.com>
2018-09-29 16:15:42 +0200

    add oversee command

---
+++ src/Command/Oversee.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\InstallationMonitor\Command;
+
+use Innmind\InstallationMonitor\Server\Local;
+use Innmind\CLI\{
+    Command,
+    Command\Arguments,
+    Command\Options,
+    Environment,
+};
+use Innmind\Server\Control\{
+    Server,
+    Server\Command as ServerCommand,
+};
+
+final class Oversee implements Command
+{
+    private $listen;
+    private $server;
+
+    public function __construct(Local $listen, Server $server)
+    {
+        $this->listen = $listen;
+        $this->server = $server;
+    }
+
+    public function __invoke(Environment $env, Arguments $arguments, Options $options): void
+    {
+        if ($options->contains('daemon')) {
+            $this
+                ->server
+                ->processes()
+                ->execute(
+                    ServerCommand::background('monitor')
+                        ->withArgument('oversee')
+                        ->withWorkingDirectory((string) $env->workingDirectory())
+                );
+
+            return;
+        }
+
+        ($this->listen)();
+    }
+
+    public function __toString(): string
+    {
+        return <<<USAGE
+oversee -d|--daemon
+
+Start a socket to collect events emitted locally by other apps
+
+The "d" option will run this command in the background
+USAGE;
+    }
+}
---
+++ tests/Command/OverseeTest.php
@@ -0,0 +1,136 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\InstallationMonitor\Command;
+
+use Innmind\InstallationMonitor\{
+    Command\Oversee,
+    Server\Local,
+};
+use Innmind\CLI\{
+    Command,
+    Command\Arguments,
+    Command\Options,
+    Environment,
+};
+use Innmind\Socket\{
+    Address\Unix as Address,
+    Loop\Strategy,
+};
+use Innmind\TimeContinuum\ElapsedPeriod;
+use Innmind\Server\Control\{
+    Server,
+    Server\Processes,
+};
+use Innmind\Url\Path;
+use Innmind\Immutable\Map;
+use PHPUnit\Framework\TestCase;
+
+class OverseeTest extends TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            Command::class,
+            new Oversee(
+                new Local(
+                    new Address('/tmp/foo'),
+                    new ElapsedPeriod(1000)
+                ),
+                $this->createMock(Server::class)
+            )
+        );
+    }
+
+    public function testUsage()
+    {
+        $usage = <<<USAGE
+oversee -d|--daemon
+
+Start a socket to collect events emitted locally by other apps
+
+The "d" option will run this command in the background
+USAGE;
+
+        $this->assertSame(
+            $usage,
+            (string) new Oversee(
+                new Local(
+                    new Address('/tmp/foo'),
+                    new ElapsedPeriod(1000)
+                ),
+                $this->createMock(Server::class)
+            )
+        );
+    }
+
+    public function testInvokation()
+    {
+        $oversee = new Oversee(
+            new Local(
+                new Address('/tmp/foo'),
+                new ElapsedPeriod(1000),
+                new class implements Strategy {
+                    public function __invoke(): bool
+                    {
+                        return false;
+                    }
+                }
+            ),
+            $server = $this->createMock(Server::class)
+        );
+        $server
+            ->expects($this->never())
+            ->method('processes');
+
+        $this->assertNull($oversee(
+            $this->createMock(Environment::class),
+            new Arguments,
+            new Options
+        ));
+    }
+
+    public function testDaemonize()
+    {
+        $oversee = new Oversee(
+            new Local(
+                new Address('/tmp/foo'),
+                new ElapsedPeriod(1000),
+                new class implements Strategy {
+                    public function __invoke(): bool
+                    {
+                        return false;
+                    }
+                }
+            ),
+            $server = $this->createMock(Server::class)
+        );
+        $server
+            ->expects($this->once())
+            ->method('processes')
+            ->willReturn($processes = $this->createMock(Processes::class));
+        $processes
+            ->expects($this->once())
+            ->method('execute')
+            ->with($this->callback(static function($command): bool {
+                return (string) $command === "monitor 'oversee'" &&
+                    $command->toBeRunInBackground() &&
+                    $command->workingDirectory() === '/tmp';
+            }));
+
+        $env = $this->createMock(Environment::class);
+        $env
+            ->expects($this->once())
+            ->method('workingDirectory')
+            ->willReturn(new Path('/tmp'));
+
+        $this->assertNull($oversee(
+            $env,
+            new Arguments,
+            new Options(
+                (new Map('string', 'mixed'))
+                    ->put('daemon', true)
+            )
+        ));
+    }
+}
