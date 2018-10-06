21220dc17305dcf7080eb7fceb503f396ea0e0e0
Baptiste Langlade <baptouuuu@gmail.com>
2018-10-06 15:14:03 +0200

    add tool to (un)serialize a set of events

---
+++ src/Events.php
@@ -0,0 +1,83 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\InstallationMonitor;
+
+use Innmind\Immutable\{
+    SequenceInterface,
+    Sequence,
+    Str,
+};
+
+final class Events implements \Iterator, \Countable
+{
+    private $events;
+    private $serialize;
+
+    public function __construct(Event ...$events)
+    {
+        $this->events = Sequence::of(...$events);
+        $this->serialize = new Serialize;
+    }
+
+    public static function from(Str $string): self
+    {
+        $unserialize = new Unserialize;
+
+        return new self(
+            ...$string
+                ->split(self::boundary())
+                ->reduce(
+                    new Sequence,
+                    static function(SequenceInterface $events, Str $event) use ($unserialize): SequenceInterface {
+                        return $events->add($unserialize($event));
+                    }
+                )
+        );
+    }
+
+    public function toString(): Str
+    {
+        return $this
+            ->events
+            ->map(function(Event $event): Str {
+                return ($this->serialize)($event);
+            })
+            ->join(self::boundary());
+    }
+
+    public function count(): int
+    {
+        return $this->events->size();
+    }
+
+    public function current(): Event
+    {
+        return $this->events->current();
+    }
+
+    public function key(): int
+    {
+        return $this->events->key();
+    }
+
+    public function next(): void
+    {
+        $this->events->next();
+    }
+
+    public function rewind(): void
+    {
+        $this->events->rewind();
+    }
+
+    public function valid(): bool
+    {
+        return $this->events->valid();
+    }
+
+    private static function boundary(): string
+    {
+        return 'ø';
+    }
+}
---
+++ tests/EventsTest.php
@@ -0,0 +1,98 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\InstallationMonitor;
+
+use Innmind\InstallationMonitor\{
+    Events,
+    Event,
+};
+use Innmind\Immutable\{
+    Map,
+    Str,
+};
+use PHPUnit\Framework\TestCase;
+
+class EventsTest extends TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(\Iterator::class, new Events);
+    }
+
+    public function testIterator()
+    {
+        $events = new Events(
+            $event1 = new Event(
+                new Event\Name('foo'),
+                new Map('string', 'variable')
+            ),
+            $event2 = new Event(
+                new Event\Name('bar'),
+                new Map('string', 'variable')
+            )
+        );
+
+        $this->assertSame([$event1, $event2], iterator_to_array($events));
+    }
+
+    public function testToString()
+    {
+        $events = new Events(
+            new Event(
+                new Event\Name('foo'),
+                new Map('string', 'variable')
+            ),
+            new Event(
+                new Event\Name('bar'),
+                new Map('string', 'variable')
+            )
+        );
+
+        $string = $events->toString();
+
+        $this->assertInstanceOf(Str::class, $string);
+        $this->assertSame(
+            '{"name":"foo","payload":[]}ø{"name":"bar","payload":[]}',
+            (string) $string
+        );
+    }
+
+    public function testSize()
+    {
+        $events = new Events(
+            new Event(
+                new Event\Name('foo'),
+                new Map('string', 'variable')
+            ),
+            new Event(
+                new Event\Name('bar'),
+                new Map('string', 'variable')
+            )
+        );
+
+        $this->assertCount(2, $events);
+    }
+
+    public function testFrom()
+    {
+        $events = Events::from(Str::of(
+            '{"name":"foo","payload":[]}ø{"name":"bar","payload":[]}'
+        ));
+
+        $this->assertInstanceOf(Events::class, $events);
+        $this->assertEquals(
+            $events,
+            new Events(
+                new Event(
+                    new Event\Name('foo'),
+                    new Map('string', 'variable')
+                ),
+                new Event(
+                    new Event\Name('bar'),
+                    new Map('string', 'variable')
+                )
+            )
+        );
+    }
+}
